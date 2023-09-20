5cfc0915e5226565106228d6cc8721464731b911
Toranks <spam@toranks.es>
2023-09-20 12:44:15 +0200

    59lvl to hard-mania and other changes
    K Rob run+s ligeramente más barato para compensar su poco alcance.
    
    3 vidas extra menos. 1 de ellas aparecerá sólo en Mania. Otra sólo si se mata a Mr Big en vez de tirarlo por la ventana (muy difícil, 5.000 de vida)
    
    Vigo agarre un poco más difícil de liberar

---
+++ data/scripts/maniapos3.c
@@ -0,0 +1,15 @@
+// Removes scroll pos effect
+void main()
+{
+    void self = getlocalvar("self"); //Get calling entity.
+    int x = getentityproperty(self,"x"); //Get character's x coordinate
+    int XPos = openborvariant("xpos");
+    void difficulty = getglobalvar("mania");
+
+    changeentityproperty(self, "position", x - XPos);
+    performattack(getlocalvar("self"), openborconstant("ANI_FOLLOW14"));
+
+    if(difficulty == 0){
+      killentity(self);
+    }
+}
