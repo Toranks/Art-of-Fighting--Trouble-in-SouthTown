c5e0355723c7ed1c576f5be94104cde1dd4de8cc
Toranks <spam@toranks.es>
2023-10-27 13:10:20 +0200

    Better code shadowtrails supercombo/power attack

--- data/scripts/player_script.c
+++ data/scripts/player_script.c
@@ -4,20 +4,14 @@ void main()
 {
 	void player = getlocalvar("self");
 	void anim = getentityproperty(player,"animationID");
-	void lv = getplayerproperty(player, "lives");
 	check_shadow_trail(player);
-
 
 	if ( anim == openborconstant("ANI_FREESPECIAL27") || anim == openborconstant("ANI_FREESPECIAL26"))
 	{
 	set_shad_trail_flag(player,1);
-    	} 
-	else if ( anim == openborconstant("ANI_FREESPECIAL27") || anim == openborconstant("ANI_FREESPECIAL26"))
+    } 
+	else if ( anim != openborconstant("ANI_FREESPECIAL27") && anim != openborconstant("ANI_FREESPECIAL26"))
 	{
- 	set_shad_trail_flag(player,0);
-	} 
-	else if ( anim != openborconstant("ANI_FREESPECIAL27") || anim == openborconstant("ANI_FREESPECIAL26"))
-	{
-        set_shad_trail_flag(player,0);
+    set_shad_trail_flag(player,0);
 	}
 }
