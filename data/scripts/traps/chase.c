908e593fbf3dc730c7311148e9a45f1203a90e1d
Toranks <spam@toranks.es>
2023-12-18 13:32:21 +0100

    Avoid traps final version
    Esta versión tiene comportamiento impecable, probablemente no requiera cambios, quizás hacerlo más eficiente, si se puede.
    Se aumenta el tiempo del fuego, incluso los molotovs, para compensar.
    El script de angelbed se beneficia de un script más rápido gracias a O Ilusionista

--- data/scripts/traps/chase.c
+++ data/scripts/traps/chase.c
@@ -1,55 +1,63 @@
-
-
 void main()
 {
     void self = getlocalvar("self");
-    void trap = findtarget(self);
-    void isfire = getentityproperty(trap, "defaultname");
- 
-    if(isfire == "potfire" || isfire == "potfire2" || isfire == "potfire3")
-    {
-        void anim = getentityproperty(self, "animationID"); //GET THE CURRENT ANIMATION
-        int z1 = getentityproperty(self, "z"); //ENEMY
-        int z2 = getentityproperty(trap, "z"); //NPC TRAP
-        int x1 = getentityproperty(self, "x"); //ENEMY
-        int x2 = getentityproperty(trap, "x"); //NPC TRAP
-        int rangeZBelow = (z1 - z2);
-        int rangeZAbove = (z2 - z1);
-        int rangeX = (x1 - x2);
 
-        
-        //CHECK IF THE ENEMY IS IN THE WALK ANIMATION
-        if(anim == openborconstant("ANI_WALK") || anim == openborconstant("ANI_RUN"))
-        {
-            //FIRE TRAP IS AT THE TOP OF THE Z BOUNDARY AND ENEMY IS BELOW (LESS THAN 33 PIXELS)
-            if(z1 > z2 && rangeZBelow < 33 && (-120 < rangeX < 120)){ //CHECK RANGE
-                changeentityproperty(self, "animation", openborconstant("ANI_JUMP")); //CHANGE ANIMATION
-                tossentity(self, 2, 0, 2);
-            }
-            //FIRE TRAP IS AT THE TOP OF THE Z BOUNDARY AND ENEMY IS BELOW (BETWEEN 33 AND 60 PIXELS)
-            else if(z1 > z2 && rangeZBelow > 33 && rangeZBelow < 60 && (-200 < rangeX < 200)){ //CHECK RANGE
-                changeentityproperty(self, "aimove", openborconstant("AIMOVE1_AVOID"));
-            }
-            //FIRE TRAP IS AT THE BOTTOM Z BOUNDARY AND ENEMY IS ABOVE (LESS THAN 33 PIXELS)
-            else if(z2 > z1 && rangeZAbove < 33 && (-120 < rangeX < 120)){ //CHECK RANGE
-                changeentityproperty(self, "animation", openborconstant("ANI_JUMP")); //CHANGE ANIMATION
-                tossentity(self, 2, 0, -2);
-            }
-            //FIRE TRAP IS AT THE BOTTOM Z BOUNDARY AND ENEMY IS ABOVE (BETWEEN 33 AND 60 PIXELS)
-            else if(z2 > z1 && rangeZAbove > 33 && rangeZBelow < 60 && (-200 < rangeX < 200)){ //CHECK RANGE
-                changeentityproperty(self, "aimove", openborconstant("AIMOVE1_AVOID"));
-            }
-            else
-            {
-                //ANY OTHER SITUATION BUT THE NEAREST TARGET IS THE FIRE TRAP
-                changeentityproperty(self, "aimove", openborconstant("AIMOVE1_NORMAL"));   
-            }
-        }
-    }
-    else
-    {
-        //ANY OTHER SITUATION
-        changeentityproperty(self, "aimove", openborconstant("AIMOVE1_CHASE"));   
-    }
+	int  iMax = openborvariant("count_entities");	//Entity count.
+	int  iEntity;				//Loop counter
+	void vEntity;				//Target entity
+
+	for(iEntity=0; iEntity<iMax; iEntity++)
+	{    
+		vEntity = getentity(iEntity);	//entity from current loop.   
+		void isfire = getentityproperty(vEntity, "defaultname");
+
+		if(isfire == "potfire" || isfire == "potfire2" || isfire == "potfire3") //IS THE ENTITY A FIRE?
+		{
+			void anim = getentityproperty(self, "animationID"); //GET THE CURRENT ANIMATION
+			int z1 = getentityproperty(self, "z"); //ENEMY
+			int z2 = getentityproperty(vEntity, "z"); //NPC TRAP
+			int x1 = getentityproperty(self, "x"); //ENEMY
+			int x2 = getentityproperty(vEntity, "x"); //NPC TRAP
+			int rangeZBelow = (z1 - z2);
+			int rangeZAbove = (z2 - z1);
+			int rangeX = (x1 - x2);
+
+			setlocalvar("thereisfire", 1); //THERE IS ANY FIRE
+			
+			//CHECK IF THE ENEMY IS IN THE WALK OR RUN ANIMATION
+			if(anim == openborconstant("ANI_WALK") || anim == openborconstant("ANI_RUN"))
+			{
+				//FIRE TRAP IS AT THE TOP OF THE Z BOUNDARY AND ENEMY IS BELOW (LESS THAN 35 PIXELS ON Z, LESS THAN 100 ON X)
+				if(z1 > z2 && rangeZBelow < 35 && (-100 < rangeX < 100)){ //CHECK RANGE
+					changeentityproperty(self, "animation", openborconstant("ANI_JUMP")); //JUMP BELOW
+					tossentity(self, 2, 0, 1);
+				}
+				//FIRE TRAP IS AT THE TOP OF THE Z BOUNDARY AND ENEMY IS BELOW (BETWEEN 35 AND 60 PIXELS ON Z, LESS THAN 120 ON X)
+				else if(z1 > z2 && 35 < rangeZBelow < 60 && (-120 < rangeX < 150)){ //CHECK RANGE
+					changeentityproperty(self, "aimove", openborconstant("AIMOVE1_AVOID")); //AVOID BEHAVIOR
+				}
+				//FIRE TRAP IS AT THE BOTTOM Z BOUNDARY AND ENEMY IS ABOVE (LESS THAN 35 PIXELS ON Z, LESS THAN 100 ON X)
+				else if(z2 > z1 && rangeZAbove < 35 && (-100 < rangeX < 100)){ //CHECK RANGE
+					changeentityproperty(self, "animation", openborconstant("ANI_JUMP")); //JUMP ABOVE
+					tossentity(self, 2, 0, -1);
+				}
+				//FIRE TRAP IS AT THE BOTTOM Z BOUNDARY AND ENEMY IS ABOVE (BETWEEN 35 AND 60 PIXELS ON Z, LESS THAN 120 ON X)
+				else if(z2 > z1 && (35 < rangeZAbove < 60) && (-120 < rangeX < 120)){ //CHECK RANGE
+					changeentityproperty(self, "aimove", openborconstant("AIMOVE1_AVOID")); //AVOID BEHAVIOR
+				}
+				//FIRE TRAP IS FURTHER THAN 60 PIXELS ON Z, OR 120 ON X
+				else if(rangeZBelow > 60 || rangeZAbove > 60 || rangeX > 120 || rangeX < -120) //CHECK RANGE
+				{
+					changeentityproperty(self, "aimove", openborconstant("AIMOVE1_NORMAL")); //NORMAL BEHAVIOR
+				}
+			}
+		}
+	}
+	if (getlocalvar("thereisfire") != 1){
+		changeentityproperty(self, "aimove", openborconstant("AIMOVE1_CHASE")); //DEFAULT BEHAVIOR		
+	}
+	else
+	{
+		setlocalvar("thereisfire", 0); //RESET FIRE COUNT
+	}
 }
-
