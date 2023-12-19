d7c8f30f24437f2a2c62e8e05c41071ab408742a
Toranks <spam@toranks.es>
2023-12-19 20:40:24 +0100

    Cleaning scripts and memory
    https://chronocrash.com/forum/threads/minimizing-memory-usage-from-script.2726/

--- data/scripts/common/nextanim.c
+++
@@ -1,7 +0,0 @@
-void nextAnim(void ent,void time)
-{
-	int enextanim = getentityproperty(ent, "nextanim");
-	int enextthink = getentityproperty(ent, "nextthink");
-	changeentityproperty(ent, "nextanim", enextanim + 80);
-	changeentityproperty(ent, "nextthink", enextthink + 80);
-}
--- data/scripts/common/spawnent.c
+++
@@ -1,7 +0,0 @@
-void spawnEnt(void vName,void x,void z)
-{
-	clearspawnentry();
-	setspawnentry("name", vName);
-	setspawnentry("coords",x,z);
-	return spawn();
-}
